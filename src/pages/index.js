f347d9ebc81254107ce77c28825082e10adc7215
Leo Torres <leohtor@gmail.com>
2019-01-03 10:44:16 -0800

    Added missing block quote to initial state

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default class IndexPage extends Component {
     super(props)
 
     this.state = {
-      textarea: ' ``` \n code block \n ```    \n  `code` \n # Hello World \n ## Hello World \n ### Hello World \n --- \n [Im an inline-style link](https://www.google.com) \n\n ---  \n * list item\n --- \n ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1") \n __bold__ '
+      textarea: ' ``` \n code block \n ```    \n  `code` \n # Hello World \n ## Hello World \n ### Hello World \n --- \n [Im an inline-style link](https://www.google.com) \n\n ---  \n * list item\n --- \n ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1") \n __bold__ \n > Block quote'
     }
     this.handleChange = this.handleChange.bind(this)
 
